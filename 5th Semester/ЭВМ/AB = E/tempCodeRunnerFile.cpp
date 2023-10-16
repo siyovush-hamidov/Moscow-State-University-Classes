39ac12c6dd08ac495ce83dab363afef2ce662d8e
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-10-16 19:04:38 +0500

    Delete 5th Semester/ЭВМ directory

--- 5th Semester/ЭВМ/AB = E/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-        // for(j = i + 1; j < n; j++)
-        // {
-        //     temp = A[j * n + i];
-        //     for(k = i; k < n; k++)
-        //         A[j * n + k] -= temp * A[i * n + k];
-        //         B[j * n + k] -= temp * B[i * n + k];
-        // }
